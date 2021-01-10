37ee4bff6b66dd15a6d4314cb323aa48b729c688
MariuszMarczewski <mariusz.marczewski1@poczta.fm>
2021-01-10 14:47:17 +0100

    technical commit

--- src/test/java/pl/mmarczewski/rentacar/controller/CarControllerTest.java
+++ src/test/java/pl/mmarczewski/rentacar/controller/CarControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import pl.mmarczewski.rentacar.model.Car;
@@ -47,8 +48,4 @@ class CarControllerTest {
         mockMvc.perform(get("/cars/1", 1L))
                 .andExpect(status().isOk());
     }
-
-
-
-
 }
